dda041688a58e3766f8526ae184661f5bc3f8084
KaminsLab <55655435+KaminsLab@users.noreply.github.com>
2022-03-13 11:15:06 +0300

    Implement BlogArticle. Add initial migration.

---
+++ module05/Northwind.Services.EntityFrameworkCore.Blogging/Context/BloggingContext.cs
@@ -0,0 +1,22 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Northwind.Services.EntityFrameworkCore.Blogging.Entities;
+
+namespace Northwind.Services.EntityFrameworkCore.Blogging.Context
+{
+    public class BloggingContext : DbContext
+    {
+        public BloggingContext(DbContextOptions<BloggingContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BlogArticle> BlogArticles { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ module05/Northwind.Services.EntityFrameworkCore.Blogging/Entities/BlogArticle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Northwind.Services.EntityFrameworkCore.Blogging.Entities
+{
+    public class BlogArticle
+    {
+        [Column("blog_article_id")]
+        public int Id { get; set; }
+
+        [Column("title")]
+        public string Title { get; set; }
+
+        [Column("content")]
+        public string Content { get; set; }
+
+        [Column("publication_date")]
+        public DateTime PublicationDate { get; set; }
+
+        [Column("employee_id")]
+        public int EmployeeId { get; set; }
+    }
+}
