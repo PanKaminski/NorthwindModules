85259c57db84416d86076ffe28bb2f7f48cea943
KaminsLab <55655435+KaminsLab@users.noreply.github.com>
2022-04-27 02:09:02 +0300

    Add client code for edit comment.

---
+++ module06/NorthwindWebApps/NorthwindApp.FrontEnd.Mvc/ViewModels/Blogging/BlogCommentEditViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace NorthwindApp.FrontEnd.Mvc.ViewModels.Blogging
+{
+    public class BlogCommentEditViewModel : BlogCommentInputViewModel
+    {
+        public int CommentId { get; set; }
+    }
+}
